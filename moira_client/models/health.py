13bddb2ff3ace916dce2aec3777b5b9bf04492cb
kamaev <hakariaruki@protonmail.com>
2018-08-15 16:22:11 +0500

    added some fixes

--- moira_client/models/health.py
+++ moira_client/models/health.py
@@ -1,32 +1,56 @@
-STATE_OK: "OK"
-STATE_ERROR: "ERROR"
+from ..client import ResponseStructureError
 
 
 class HealthManager:
     def __init__(self, client):
         self._client = client
 
+    def get_notifier_state(self):
+        """
+        Returns current Moira Notifier state
+        :return: str
+
+        :raises: ResponseStructureError
+        """
+        result = self._client.get(self._full_path("notifier"))
+        if 'state' not in result:
+            raise ResponseStructureError("state doesn't exist in response", result)
+
+        return result['state']
+
     def disable_notifier(self):
         """
         Manages Moira Notifier to stop sending notifications
-        :return: bool
+        Returns current Moira Notifier state
+        :return: str
+
+        :raises: ResponseStructureError
         """
-        params = {
-            'state': STATE_ERROR
+        data = {
+            'state': "ERROR"
         }
-        result = self._client.get(self._full_path("notifier"))
-        return result
+        result = self._client.put(self._full_path("notifier"), json=data)
+        if 'state' not in result:
+            raise ResponseStructureError("state doesn't exist in response", result)
+
+        return result['state']
 
     def enable_notifier(self):
         """
         Manages Moira Notifier to start sending notifications
-        :return: bool
+        Returns current Moira Notifier state
+        :return: str
+
+        :raises: ResponseStructureError
         """
-        params = {
-            'state': STATE_OK
+        data = {
+            'state': "OK"
         }
-        result = self._client.get(self._full_path("notifier"))
-        return result
+        result = self._client.put(self._full_path("notifier"), json=data)
+        if 'state' not in result:
+            raise ResponseStructureError("state doesn't exist in response", result)
+
+        return result['state']
 
     def _full_path(self, path=''):
         if path:
