c618b8c6dd49a56be7bc7ba90ca2bd3d5d107e76
Timur Kamaev <hakariaruki@protonmail.com>
2018-08-16 16:48:25 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='moira-client',
-    version='2.3',
+    version='2.3.2',
     description='Client for Moira - Alerting system based on Graphite data',
     keywords='moira monitoring client metrics alerting',
     long_description="""
